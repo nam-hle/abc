fc979fc27a25e3f02b1a91a68713cc9cb43368f4
Nam Hoang Le <nam.hoang.le@mgm-tp.com>
2021-05-23 12:40:02 +0700

    Deploy website - based on 5a2dd0a675c2157f02f831e4c769c88abfc290dc

---
+++ assets/js/537a34dc.ebd297d9.js
@@ -0,0 +1,1 @@
+(self.webpackChunkabc=self.webpackChunkabc||[]).push([[180],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"},{"title":"Hello","permalink":"/blog/hello-world"},{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
